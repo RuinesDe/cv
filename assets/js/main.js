12720ff62b00891de9bd5507a6c1ad4b9b58f80b
RuinesDe <mel.briere@wanadoo.fr>
2019-10-27 13:01:20 +0100

    mise à jour octobre 2019

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,40 +4,23 @@
   var wipLink = document.getElementById("wipLink");
   var wipIllustrations = document.getElementById("wipIllustrations");
 
-  var firstClick = false;
+  var wipIllustrationsOpen = false;
   var picture = "";
   var previousPicture = "";
-  var previousIndex = 0;
+  // var previousIndex = 0;
 
   wipLink.addEventListener("click", function(e) {
     e.preventDefault();
 
-    var randomIndex = Math.floor(Math.random() * 5) + 1;
+    // var randomIndex = Math.floor(Math.random() * 5) + 1;
 
-    if (firstClick === false) {
-      firstClick = true;
+    if (wipIllustrationsOpen === false) {
+      wipIllustrationsOpen = true;
       wipIllustrations.classList.add("visible");
     } else {
-      previousPicture.classList.remove("visible");
+      wipIllustrationsOpen = false;
+      wipIllustrations.classList.remove("visible");
     }
-
-    if (randomIndex !== previousIndex ) {
-      picture = document.getElementById("picture"+randomIndex);
-    } else {
-      if (randomIndex === 5) {
-        randomIndex = 4;
-      } else if (randomIndex === 1) {
-        randomIndex = 2;
-      } else {
-        randomIndex = randomIndex - 1;
-      }
-
-      picture = document.getElementById("picture"+randomIndex);
-    }
-
-    previousIndex = randomIndex;
-    previousPicture = picture;
-    picture.classList.add("visible");
   }, false);
 
 })();
